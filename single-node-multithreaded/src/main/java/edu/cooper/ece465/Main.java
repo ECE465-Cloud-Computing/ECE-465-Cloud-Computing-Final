314001c4ab85576689735c4fa98c12c89f4c3248
Eric <xue@cooper.edu>
2021-04-02 14:06:22 -0400

    Started networking

--- single-node-multithreaded/src/main/java/edu/cooper/ece465/Main.java
+++ single-node-multithreaded/src/main/java/edu/cooper/ece465/Main.java
@@ -1,27 +1,52 @@
 package edu.cooper.ece465;
 
-import java.io.IOException;
+import java.io.*;
+import java.net.Socket;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) {
         String host_ip = args[0];
         int portnum = Integer.parseInt(args[1]);
 
-        Graph graph1 = Util.readGraph("1_$cost.txt");
-        Graph graph2 = Util.readGraph("1_time.txt");
+//        Graph graph1 = Util.readGraph("1_$cost.txt");
+//        Graph graph2 = Util.readGraph("1_time.txt");
 
 
-        List<Integer> results = runDijkstra(graph1, graph2, 2);
+        List<Integer> results = runDijkstra(null, null, 2, host_ip, portnum);
 
-        Util.writeResults("singleoutput.txt", results, 0);
+//        Util.writeResults("singleoutput.txt", results, 0);
     }
 
-    private static List<Integer> runDijkstra(Graph graph1, Graph graph2, int numThreads) throws InterruptedException {
+    private static List<Integer> runDijkstra(Graph graph1, Graph graph2, int numThreads, String host_ip, int portnum) {
         Dijkstra dijkstra = new Dijkstra();
-        return dijkstra.runAlgo(graph1, numThreads);
+
+        try {
+            Socket s = new Socket(host_ip, portnum);
+            System.out.println("Connection establish with " + host_ip + "::" + portnum);
+            network_runalgo(graph1, graph2, numThreads, s);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+//        return dijkstra.runAlgo(graph1, numThreads);
+
+    }
+
+    private static void network_runalgo(Graph graph1, Graph graph2, int numThreads, Socket s) throws IOException {
+        DataOutputStream dataOutputStream = new DataOutputStream(s.getOutputStream());
+        DataInputStream dataInputStream = new DataInputStream(s.getInputStream());
+
+        String rec_msg = dataInputStream.readUTF();
+        System.out.println("Received_Message: " + rec_msg);
+
+        dataOutputStream.writeUTF("Sending Message.");
+        dataOutputStream.flush();
+
+        System.out.println("Finished receiving and sending.");
+
     }
 }
