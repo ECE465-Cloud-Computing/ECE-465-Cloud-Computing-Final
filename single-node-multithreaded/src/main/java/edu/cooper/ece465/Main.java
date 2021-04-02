853c3e1d2bafcdb67a791730c625f81dceca360b
Eric <xue@cooper.edu>
2021-04-02 17:45:07 -0400

    More scripting + AWS VPC private ipv4 communication

--- single-node-multithreaded/src/main/java/edu/cooper/ece465/Main.java
+++ single-node-multithreaded/src/main/java/edu/cooper/ece465/Main.java
@@ -1,34 +1,37 @@
 package edu.cooper.ece465;
 
 import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
-        String host_ip = args[0];
-        int portnum = Integer.parseInt(args[1]);
+//        String host_ip = args[0];
+        int portnum = Integer.parseInt(args[0]);
 
 //        Graph graph1 = Util.readGraph("1_$cost.txt");
 //        Graph graph2 = Util.readGraph("1_time.txt");
 
 
-        List<Integer> results = runDijkstra(null, null, 2, host_ip, portnum);
+        List<Integer> results = runDijkstra(null, null, 2, portnum);
 
 //        Util.writeResults("singleoutput.txt", results, 0);
     }
 
-    private static List<Integer> runDijkstra(Graph graph1, Graph graph2, int numThreads, String host_ip, int portnum) {
+    private static List<Integer> runDijkstra(Graph graph1, Graph graph2, int numThreads, int portnum) {
         Dijkstra dijkstra = new Dijkstra();
 
         try {
-            Socket s = new Socket(host_ip, portnum);
-            System.out.println("Connection establish with " + host_ip + "::" + portnum);
+            ServerSocket serverSocket = new ServerSocket(portnum);
+            Socket s = serverSocket.accept();
+            System.out.println("Connection establish with on port: " + portnum);
             network_runalgo(graph1, graph2, numThreads, s);
-        } catch (IOException e) {
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
         return null;
@@ -36,7 +39,7 @@ public class Main {
 
     }
 
-    private static void network_runalgo(Graph graph1, Graph graph2, int numThreads, Socket s) throws IOException {
+    private static void network_runalgo(Graph graph1, Graph graph2, int numThreads, Socket s) throws IOException, InterruptedException {
         DataOutputStream dataOutputStream = new DataOutputStream(s.getOutputStream());
         DataInputStream dataInputStream = new DataInputStream(s.getInputStream());
 
@@ -47,6 +50,5 @@ public class Main {
         dataOutputStream.flush();
 
         System.out.println("Finished receiving and sending.");
-
     }
 }
