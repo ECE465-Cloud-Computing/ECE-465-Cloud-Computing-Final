2c860163416a4da037dd216b52826360bfa082b1
Eric <xue@cooper.edu>
2021-04-09 14:53:44 -0400

    Changes to Coord

--- single-node-multithreaded/src/main/java/edu/cooper/ece465/CoordinatorMain.java
+++ single-node-multithreaded/src/main/java/edu/cooper/ece465/CoordinatorMain.java
@@ -3,12 +3,15 @@ package edu.cooper.ece465;
 public class CoordinatorMain {
     public static void main(String[] args) {
         int portNumber = Integer.parseInt(args[0]);
+        String filter = args[1];
+        int start = Integer.parseInt(args[2]);
+        int end = Integer.parseInt(args[3]);
         String[] workerIP = new String[args.length-1];
-        for(int i=1; i < args.length; i++) {
-            workerIP[i-1] = args[i];
+        for(int i=4; i < args.length-1; i++) {
+            workerIP[i-4] = args[i];
         }
 
-        Coordinator coordinator = new Coordinator(workerIP, portNumber);
+        Coordinator coordinator = new Coordinator(workerIP, portNumber, filter, start, end);
         coordinator.test();
     }
 }
