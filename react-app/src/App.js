beb01eced2432fcead5d2e95a69e809061ff6584
jplulu <lujason899@gmail.com>
2021-05-01 13:50:13 -0400

    Login/register init

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import "./App.css";
+import Home from "./components/Home";
+import Login from './components/Login.js'
+import Register from "./components/Register";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route path="/" exact component={Home}/>
+                <Route path="/register" exact component={Register}/>
+                <Route path="/login" exact component={Login}/>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
---
+++ react-app/src/components/Home.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import loggedIn from "./loggedIn";
+import Login from "./Login";
+
+class Home extends Component {
+    componentDidMount() {
+        if (loggedIn()) {
+            const user_type = JSON.parse(localStorage.getItem("user")).user_type;
+            if (user_type === "ADMIN") {
+                this.props.history.push("/admin");
+            } else if (user_type === "PATIENT") {
+                this.props.history.push("/patient");
+            } else {
+                this.props.history.push("/doctor");
+            }
+        }
+    }
+
+    render() {
+        return (
+            <div style={{ textAlign: "center" }}>
+                <h1>Welcome</h1>
+                <Login></Login>
+            </div>
+        );
+    }
+}
+
+export default Home;
